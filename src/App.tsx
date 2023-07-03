5a63546f3ae44f4c1b701395efcca678b801880c
parkyejin <uiop5809@naver.com>
2023-07-03 23:46:43 +0900

    Chore: recoil 설치, 설정

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,24 @@ import MapPage from './routes/MapPage';
 import MissionPage from './routes/MissionPage';
 import MissionAddPage from './routes/MissionAddPage';
 import MyPage from './routes/MyPage';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <Routes>
-        <Route path='/' element={<SplashPage />} />
-        <Route path='/user' element={<UserPage />} />
-        <Route path='/main' element={<MainPage />} />
-        <Route path='/map' element={<MapPage />} />
-        <Route path='/mission' element={<MissionPage />}>
-          <Route path='add' element={<MissionAddPage />} />
-        </Route>
-        <Route path='/mypage' element={<MyPage />} />
-      </Routes>
+      <RecoilRoot>
+        <GlobalStyle />
+        <Routes>
+          <Route path='/' element={<SplashPage />} />
+          <Route path='/user' element={<UserPage />} />
+          <Route path='/main' element={<MainPage />} />
+          <Route path='/map' element={<MapPage />} />
+          <Route path='/mission' element={<MissionPage />}>
+            <Route path='add' element={<MissionAddPage />} />
+          </Route>
+          <Route path='/mypage' element={<MyPage />} />
+        </Routes>
+      </RecoilRoot>
     </>
   );
 }
