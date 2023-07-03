e0bf48da08e3a90ebbaedc51506bcc68d6fc8ed4
parkyejin <uiop5809@naver.com>
2023-07-03 22:21:40 +0900

    Feat: #17 메인 페이지 작성

--- src/components/main/Recommend.tsx
+++ src/components/main/Recommend.tsx
@@ -46,14 +46,16 @@ const RecommendBox = styled.button`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  width: 100%;
-  height: 20%;
+  width: 90%;
+  height: 8rem;
   border: 1px solid ${COLOR.GRAY};
   padding: 1.2rem 1rem;
+  margin-bottom: 1rem;
 `;
 
 const RecommendLeft = styled.div`
   width: 25%;
+  height: 100%;
   margin-right: 1rem;
 `;
 
--- src/routes/MainPage.tsx
+++ src/routes/MainPage.tsx
@@ -1,9 +1,49 @@
+import styled from 'styled-components';
+import Recommend from '../components/main/Recommend';
+import { Container } from '../styles/Container';
+
 const MainPage = () => {
   return (
-    <div>
-      <h1>MainPage</h1>
-    </div>
+    <Container>
+      <MainContainer>
+        <Recommend
+          src={require('../assets/여수.jpg')}
+          title={'여수여행 좋아요'}
+          subtitle={'여수 여행 추천합니다!! 완전 강추'}
+          tag1={'# 여수'}
+          tag2={'# 바다'}
+        />
+        <Recommend
+          src={require('../assets/순천.jpg')}
+          title={'순천여행 좋아요'}
+          subtitle={'순천 여행 추천합니다!! 완전 강추'}
+          tag1={'# 순천'}
+          tag2={'# 바다'}
+        />
+        <Recommend
+          src={require('../assets/여수.jpg')}
+          title={'여수여행 좋아요'}
+          subtitle={'여수 여행 추천합니다!! 완전 강추'}
+          tag1={'# 여수'}
+          tag2={'# 바다'}
+        />
+        <Recommend
+          src={require('../assets/순천.jpg')}
+          title={'순천여행 좋아요'}
+          subtitle={'순천 여행 추천합니다!! 완전 강추'}
+          tag1={'# 순천'}
+          tag2={'# 바다'}
+        />
+      </MainContainer>
+    </Container>
   );
 };
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default MainPage;
