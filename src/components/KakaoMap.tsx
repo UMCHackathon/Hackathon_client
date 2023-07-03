d7019595acb9a49ce256707435c35741b9c349ce
rbgksqkr <rbgks1937@naver.com>
2023-07-03 21:09:36 +0900

    Feat: #1 - 현재 위치 기준으로 지도 표시

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -9,26 +9,16 @@ declare global {
 const { kakao } = window;
 
 const KakaoMap = () => {
-  const [maps, setMaps] = useState<any>();
-  const [markers, setMarkers] = useState<any>();
-  // const [location, setLocation] = useLocation();
+  const [maps, setMaps] = useState();
+  const [markers, setMarkers] = useState();
+  const [location, setLocation] = useState({ latitude: 0, longitude: 0 });
 
-  // 3) 정상적으로 현재위치 가져올 경우 실행
   const getPosSuccess = async (pos: GeolocationPosition) => {
     // 현재 위치(위도, 경도) 가져온다.
-    const currentPos = new kakao.maps.LatLng(
-      pos.coords.latitude, // 위도
-      pos.coords.longitude // 경도
-    );
-    // currentPos.La, currentPos.Ma
-    console.log('currentPos:', currentPos);
-    // // 지도를 이동 시킨다.
-    maps.panTo(currentPos);
-
-    // // 기존 마커를 제거하고 새로운 마커를 넣는다.
-    markers.setMap(null);
-    markers.setPosition(currentPos);
-    markers.setMap(maps);
+    setLocation({
+      latitude: pos.coords.latitude,
+      longitude: pos.coords.longitude
+    });
   };
 
   const getCurrentPosBtn = async () => {
@@ -42,24 +32,33 @@ const KakaoMap = () => {
       }
     );
   };
+  useEffect(() => {
+    getCurrentPosBtn();
+  }, []);
 
   useEffect(() => {
     window.kakao.maps.load(() => {
       const container = document.getElementById('map');
       const options = {
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        center: new window.kakao.maps.LatLng(
+          location.latitude,
+          location.longitude
+        ),
         level: 3
       };
       setMaps(new window.kakao.maps.Map(container, options));
       setMarkers(new window.kakao.maps.Marker());
+      //   if (markers && maps) {
+      //     markers.setPosition(options.center);
+      //     markers.setMap(maps);
+      //   }
     });
-  }, []);
+  }, [location]);
 
   return (
     <div className='App'>
       <div>카카오맵 API</div>
       <div id='map' style={{ width: '500px', height: '500px' }}></div>
-      <button onClick={getCurrentPosBtn}>현재위치</button>
     </div>
   );
 };
