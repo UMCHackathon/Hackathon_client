b3d497f55e1a2dcc0df9f05400bdde81209867ee
alreadynyeong <kjw87130@naver.com>
2023-07-03 20:52:59 +0900

    #6 Feat: 미션 댓글 컴포넌트 생성

---
+++ src/components/mission/comment.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+import COLOR from '../../constants/colors';
+import FONT from '../../constants/fonts';
+import { CommentProps } from '../../types/comment';
+import { ReactComponent as DeleteButton } from '../../asset/svg/DeleteButton.svg';
+
+const Comment = (data:CommentProps) => {
+    return (
+        <CommentBox>
+            <Top>
+                <Content style={FONT.SUBTITLE4}>{data.content}</Content>
+                <DeleteBtnBox><DeleteButton /></DeleteBtnBox>
+            </Top>
+            <div style={FONT.SUBTITLE5}>{data.nickName} | {data.createdAt}</div>
+        </CommentBox>
+    )
+}
+
+const CommentList: React.FC<{ data: CommentProps[] }> = ({ data }) => {
+    return (
+      <div>
+        {data.map((comment) => (
+          <div key={comment.id}>
+            <Comment {...comment} />
+          </div>
+        ))}
+      </div>
+    );
+  };
+  
+export default CommentList;
+
+const CommentBox = styled.div`
+    width: 300px;
+    height: fit-content;
+    padding: 10px;
+    border-bottom: 0.4px solid ${COLOR.GREEN1};
+
+`
+const Content = styled.div`
+    margin-bottom: 5px;
+    width: 90%;
+`
+
+const Top = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const DeleteBtnBox = styled.div`
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+`
---
+++ src/types/comment.ts
@@ -0,0 +1,6 @@
+export interface CommentProps {
+    id: number;
+    content: string;
+    nickName: string;
+    createdAt: string;
+}
