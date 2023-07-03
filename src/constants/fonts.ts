ca0d1a1cb065941de3627275f42536085af0127e
parkyejin <uiop5809@naver.com>
2023-07-03 19:56:16 +0900

    Style: font 크기 단위 재설정

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -1,39 +1,39 @@
 import { CSSProperties } from 'react';
 
 const HEADING: CSSProperties = {
-  fontSize: '2.4rem',
+  fontSize: '24px',
   fontWeight: '600',
   lineHeight: '150%',
   letterSpacing: '-0.4px'
 };
 
 const SUBTITLE1: CSSProperties = {
-  fontSize: '2rem',
+  fontSize: '20px',
   fontWeight: '500',
   lineHeight: '140%',
   letterSpacing: '-0.4px'
 };
 
 const SUBTITLE2: CSSProperties = {
-  fontSize: '1.6rem',
+  fontSize: '16px',
   fontWeight: '500',
   lineHeight: '140%'
 };
 
 const SUBTITLE3: CSSProperties = {
-  fontSize: '1.4rem',
+  fontSize: '14px',
   fontWeight: '500',
   lineHeight: '140%'
 };
 
 const SUBTITLE4: CSSProperties = {
-  fontSize: '1.2rem',
+  fontSize: '12px',
   fontWeight: '400',
   lineHeight: '160%'
 };
 
 const SUBTITLE5: CSSProperties = {
-  fontSize: '1rem',
+  fontSize: '10px',
   fontWeight: '400',
   lineHeight: '160%'
 };
