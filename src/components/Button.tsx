950cc730771260e7ecc5b37f5e7e21d65d0b3fd5
alreadynyeong <kjw87130@naver.com>
2023-07-03 19:54:58 +0900

    #2 Style: 버튼 컴포넌트 스타일 시스템 적용

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import COLOR from '../constants/colors';
+import FONT from '../constants/fonts';
 
 interface ButtonProps {
     text?: string;
@@ -11,16 +13,16 @@ const Button = ({
 }: ButtonProps) => {
     return (
         <div>
-        <StyledButton onClick={onClick}>{text}</StyledButton>
+        <StyledButton style={FONT.SUBTITLE2} onClick={onClick}>{text}</StyledButton>
         </div>
     );
 };
 
 export const StyledButton = styled.button`
-    background-color: #36A400;
-    width: 274px;
+    background-color: ${COLOR.GREEN2};
+    width: 100%;
     height: 45px;
-    color: #FFFFFF;
+    color: #fff;
     border: none;
     border-radius: 12px;
 `
