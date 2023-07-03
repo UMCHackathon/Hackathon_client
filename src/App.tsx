ca6d1ae714e67e9705a84be9acf97d962f49ed76
parkyejin <uiop5809@naver.com>
2023-07-03 21:16:44 +0900

    Feat: #12 페이지 설정 react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import GlobalStyle from './styles/GlobalStyle';
-import Recommend from './components/Recommend';
+import { Routes, Route } from 'react-router-dom';
+import SplashPage from './routes/SplashPage';
+import UserPage from './routes/UserPage';
+import MainPage from './routes/MainPage';
+import MapPage from './routes/MapPage';
+import MissionPage from './routes/MissionPage';
+import MissionAddPage from './routes/MissionAddPage';
+import MyPage from './routes/MyPage';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      {/* 컴포넌트 예시 
-      <Recommend
-        title={'여수여행 좋아요요'}
-        subtitle={'여수 여행 추천합니다!! 완전 강추추'}
-        tag1={'#여수'}
-        tag2={'#바다'}
-        src={require('./assets/여수.jpg')}
-      /> */}
+      <Routes>
+        <Route path='/' element={<SplashPage />} />
+        <Route path='/user' element={<UserPage />} />
+        <Route path='/main' element={<MainPage />} />
+        <Route path='/map' element={<MapPage />} />
+        <Route path='/mission' element={<MissionPage />}>
+          <Route path='add' element={<MissionAddPage />} />
+        </Route>
+        <Route path='/mypage' element={<MyPage />} />
+      </Routes>
     </>
   );
 }
---
+++ src/routes/MapPage.tsx
@@ -0,0 +1,9 @@
+const MapPage = () => {
+  return (
+    <div>
+      <h1>MapPage</h1>
+    </div>
+  );
+};
+
+export default MapPage;
