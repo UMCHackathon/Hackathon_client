d7434ccfff09da44f895a456ad6e71460aadcc66
parkyejin <uiop5809@naver.com>
2023-07-03 22:34:23 +0900

    Design: 메인 페이지 디자인 수정

--- src/routes/MainPage.tsx
+++ src/routes/MainPage.tsx
@@ -2,11 +2,14 @@ import styled from 'styled-components';
 import Recommend from '../components/main/Recommend';
 import { Container } from '../styles/Container';
 import Header from '../components/Header';
+import { ReactComponent as MainPost } from '../assets/svg/MainPost.svg';
+import COLOR from '../constants/colors';
 
 const MainPage = () => {
   return (
     <Container>
       <Header />
+      <MainPostBox></MainPostBox>
       <MainContainer>
         <Recommend
           src={require('../assets/여수.jpg')}
@@ -29,6 +32,13 @@ const MainPage = () => {
           tag1={'# 여수'}
           tag2={'# 바다'}
         />
+        <Recommend
+          src={require('../assets/순천.jpg')}
+          title={'순천여행 좋아요'}
+          subtitle={'순천 여행 추천합니다!! 완전 강추'}
+          tag1={'# 순천'}
+          tag2={'# 바다'}
+        />
       </MainContainer>
     </Container>
   );
@@ -39,6 +49,13 @@ const MainContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+const MainPostBox = styled.div`
+  width: 100%;
+  height: 14rem;
+  background: ${COLOR.GREEN3};
+  margin-bottom: 2rem;
 `;
 
 export default MainPage;
